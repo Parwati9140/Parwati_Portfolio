fab937d19b533304ef4fa76b04335d4363b80831
parwati <parwatimaurya9721@gmail.com>
2024-10-14 17:15:49 +0530

    hi

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -17,6 +17,21 @@ const skills = [
     { name: 'Mysql', image: `${process.env.PUBLIC_URL}/images/mysql.svg` },
     { name: 'PostgreSQL', image: `${process.env.PUBLIC_URL}/images/postgresql.svg` },
     { name: 'Canva', image: `${process.env.PUBLIC_URL}/images/canva.svg` },
+    { name: 'MaterialUi', image: `${process.env.PUBLIC_URL}/images/materialui.svg` },
+    { name: 'React', image: `${process.env.PUBLIC_URL}/images/react.svg` },
+    { name: 'Html', image: `${process.env.PUBLIC_URL}/images/html.svg` },
+    { name: 'Css', image: `${process.env.PUBLIC_URL}/images/css.svg` },
+    { name: 'Javascript', image: `${process.env.PUBLIC_URL}/images/javaScript.svg` },
+    { name: 'Bootstrap', image: `${process.env.PUBLIC_URL}/images/bootstrap.svg` },
+    { name: 'Mongodb', image: `${process.env.PUBLIC_URL}/images/mongoDb.svg` },
+    { name: 'Node', image: `${process.env.PUBLIC_URL}/images/mongoDb.svg` }, 
+    { name: 'Git', image: `${process.env.PUBLIC_URL}/images/git.svg` },
+    { name: 'Figma', image: `${process.env.PUBLIC_URL}/images/figma.svg` },
+    { name: 'Java', image: `${process.env.PUBLIC_URL}/images/java.svg` },
+    { name: 'Php', image: `${process.env.PUBLIC_URL}/images/php.svg` },  
+    { name: 'Mysql', image: `${process.env.PUBLIC_URL}/images/mysql.svg` },
+    { name: 'PostgreSQL', image: `${process.env.PUBLIC_URL}/images/postgresql.svg` },
+    { name: 'Canva', image: `${process.env.PUBLIC_URL}/images/canva.svg` },
     { name: 'MaterialUi', image: `${process.env.PUBLIC_URL}/images/materialui.svg` }
   
 ];
